10d8efe659a274a42ed7a1f699916fc5b9e2f75b
John Previtera <johngprevitera@gmail.com>
2024-04-10 14:24:39 -0600

    created rat model

---
+++ src/models/Rat.js
@@ -0,0 +1,9 @@
+import { Schema } from "mongoose"
+
+
+export const RatSchema = new Schema({
+    name: { type: String },
+    callsign: { type: String },
+    picture: { type: String },
+    specialties: []
+})
