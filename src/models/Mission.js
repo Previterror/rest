a2198600ded93e9a4f026543d430e3957265f5ee
John Previtera <johngprevitera@gmail.com>
2024-04-10 14:34:56 -0600

    finished mission schema and registered in dbcontext

---
+++ src/models/Mission.js
@@ -0,0 +1,10 @@
+import { Schema } from "mongoose";
+
+
+export const MissionSchema = new Schema({
+    codename: { type: String },
+    objective: { type: String },
+    year: { type: String },
+    locationId: { type: Schema.ObjectId, ref: 'Location' },
+    ratId: { type: Schema.ObjectId, ref: 'Rat' }
+})
